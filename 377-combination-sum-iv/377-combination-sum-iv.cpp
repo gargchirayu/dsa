1cdb45317bd8ee38b75c2252b1d160ef292bc80c
Chirayu Garg <11246023+gargchirayu@users.noreply.github.com>
2022-08-05 19:12:38 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,21 +1,21 @@
 class Solution {
 private:
-    int helper(int ind, int target, vector<int> &nums, vector<vector<int>> &dp){
+    int helper(int ind, int target, vector<int> &nums, vector<int> &dp){
         //base case
-        if(target == 0) return dp[ind][target] = 1;
-        if(ind == nums.size()) return dp[ind][target] = 0;
-        if(dp[ind][target] != -1) return dp[ind][target];
+        if(target == 0) return dp[target] = 1;
+        if(ind == nums.size()) return dp[target] = 0;
+        if(dp[target] != -1) return dp[target];
         int take = 0;
         for(int i=0; i<nums.size(); i++)
             if(nums[i]<=target) take += helper(0, target-nums[i], nums, dp);
         //int notTake = helper(ind+1, target, nums);
         
-        return dp[ind][target] = take;
+        return dp[target] = take;
     }
 public:
     int combinationSum4(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
+        vector<int> dp(target+1, -1);
         return helper(0, target, nums, dp);
     }
 };
