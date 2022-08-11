7a0866242178faf93d87a896e70d71d8c9a3b639
Chirayu Garg <11246023+gargchirayu@users.noreply.github.com>
2022-08-12 03:11:31 +0530

    Time: 34 ms (6.11%), Space: 21.7 MB (70.37%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -10,34 +10,34 @@
  * };
  */
 class Solution {
-// private:
-//     bool helper(TreeNode* root, long long low, long long high){
-//         //base case
-//         if(root==NULL) return true;
-        
-//         if(root->val<=low or high<=root->val) return false;
-//         bool l = helper(root->left, low, root->val);
-//         bool r = helper(root->right, root->val, high);
-        
-//         return (l and r);
-//     }
-// public:
-//     bool isValidBST(TreeNode* root) {
-//         return helper(root, INT_MIN*1LL - 1LL, INT_MAX*1LL + 1LL);
-//     }
 private:
-    bool helper(TreeNode* root, TreeNode* low, TreeNode* high){
+    bool helper(TreeNode* root, long long low, long long high){
         //base case
         if(root==NULL) return true;
         
-        if((low!=NULL and root->val<=low->val) or (high!=NULL and high->val<=root->val)) return false;
-        bool l = helper(root->left, low, root);
-        bool r = helper(root->right, root, high);
+        if(root->val<=low or high<=root->val) return false;
+        bool l = helper(root->left, low, root->val);
+        bool r = helper(root->right, root->val, high);
         
         return (l and r);
     }
 public:
     bool isValidBST(TreeNode* root) {
-        return helper(root, NULL, NULL);
+        return helper(root, INT_MIN*1LL - 1LL, INT_MAX*1LL + 1LL);
     }
+// private:
+//     bool helper(TreeNode* root, TreeNode* low, TreeNode* high){
+//         //base case
+//         if(root==NULL) return true;
+        
+//         if((low!=NULL and root->val<=low->val) or (high!=NULL and high->val<=root->val)) return false;
+//         bool l = helper(root->left, low, root);
+//         bool r = helper(root->right, root, high);
+        
+//         return (l and r);
+//     }
+// public:
+//     bool isValidBST(TreeNode* root) {
+//         return helper(root, NULL, NULL);
+//     }
 };
