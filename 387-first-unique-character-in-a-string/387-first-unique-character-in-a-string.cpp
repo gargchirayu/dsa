86c100cfd403614ba0eaaebc62d994eb3d5bf9c4
Chirayu Garg <11246023+gargchirayu@users.noreply.github.com>
2022-08-16 17:08:42 +0530

    Time: 42 ms (70.12%), Space: 10.6 MB (91.52%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> mp;
+        // unordered_map<char, int> mp;
+        // for(int i=0; i<s.size(); i++){
+        //     if(mp.find(s[i])!=mp.end()) mp[s[i]] = -1;
+        //     else mp.insert({s[i], i});
+        // }
+        // int ans=INT_MAX;
+        // for(auto it:mp){
+        //     if(it.second != -1) ans = min(ans, it.second);
+        // }
+        // if(ans==INT_MAX) return -1;
+        // return ans;
+        
+        vector<int> dp(26, 1e6);
         for(int i=0; i<s.size(); i++){
-            if(mp.find(s[i])!=mp.end()) mp[s[i]] = -1;
-            else mp.insert({s[i], i});
+            if(dp[s[i]-97] != 1e6) dp[s[i]-97] = -1;
+            else dp[s[i]-97] = i;
         }
-        int ans=INT_MAX;
-        for(auto it:mp){
-            if(it.second != -1) ans = min(ans, it.second);
+        int ans = 1e6;
+        for(int i=0; i<26; i++){
+            if(dp[i] != -1) ans = min(ans, dp[i]);
         }
-        if(ans==INT_MAX) return -1;
+        if(ans == 1e6) return -1;
         return ans;
     }
 };
