0efcb4986771867af2b93cf4001bd683dae3bb9a
Chirayu Garg <11246023+gargchirayu@users.noreply.github.com>
2022-08-26 01:25:56 +0530

    Time: 7 ms (56.60%), Space: 6.8 MB (96.14%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        if(s=="") return 0;
-        stack<pair<char, int>> st;
+        // if(s=="") return 0;
+        // stack<pair<char, int>> st;
+        // for(int i=0; i<s.size(); i++){
+        //     if(!st.empty() and st.top().first == '(' and s[i] == ')') st.pop();
+        //     else st.push({s[i], i});
+        // }
+        // int ans = INT_MIN;
+        // int x = s.size();
+        // if(st.empty()) return x;
+        // while(!st.empty()){
+        //     int temp = st.top().second;
+        //     cout<<temp<<" ";
+        //     st.pop();
+        //     ans = max(x - temp - 1, ans);
+        //     x = temp;
+        //     if(st.empty()) ans = max(ans, temp);
+        // }
+        // return ans;
+        
+        int open = 0, close = 0, ans = 0;
         for(int i=0; i<s.size(); i++){
-            if(!st.empty() and st.top().first == '(' and s[i] == ')') st.pop();
-            else st.push({s[i], i});
+            if(s[i] == '(') open++;
+            else close++;
+            if(open == close) ans = max(ans, (open+close));
+            else if(close > open) open = close = 0;
         }
-        int ans = INT_MIN;
-        int x = s.size();
-        if(st.empty()) return x;
-        while(!st.empty()){
-            int temp = st.top().second;
-            cout<<temp<<" ";
-            st.pop();
-            ans = max(x - temp - 1, ans);
-            x = temp;
-            if(st.empty()) ans = max(ans, temp);
+        open = close = 0;
+        for(int i=s.size()-1; i>=0; --i){
+            if(s[i] == '(') open++;
+            else close++;
+            if(open == close) ans = max(ans, (open+close));
+            else if(open > close) open = close = 0;
         }
         return ans;
     }
