529e214148abb396f1b360a40b4500fc39efef19
Chirayu Garg <11246023+gargchirayu@users.noreply.github.com>
2022-08-28 17:08:56 +0530

    Time: 11 ms (88.97%), Space: 9.4 MB (52.67%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        for(int i=n-1; i>=0; --i){
+            vector<int> diag;
+            for(int x = i, y = 0; x<n and y<m; x++, y++){
+                diag.push_back(mat[y][x]);
+            }
+            sort(diag.begin(), diag.end());
+            for(int x = i, y = 0, k=0; x<n and y<m; x++, y++, k++){
+                mat[y][x] = diag[k];
+            }
+        }
+        for(int i=1; i<m; i++){
+            vector<int> diag;
+            for(int x = i, y = 0; x<m and y<n; x++, y++){
+                cout<<x<<"-"<<y<<" ";
+                diag.push_back(mat[x][y]);
+            }
+            cout<<".";
+            sort(diag.begin(), diag.end());
+            for(int x = i, y = 0, k=0; x<m and y<n; x++, y++, k++){
+                mat[x][y] = diag[k];
+            }
+        }
+        return mat;
+    }
+};
