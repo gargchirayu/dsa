1b3b8e13504357a5d88278c9720e62fa654d08dd
Chirayu Garg <11246023+gargchirayu@users.noreply.github.com>
2022-08-28 17:15:39 +0530

    Time: 66 ms (5.15%), Space: 9.3 MB (52.67%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -28,4 +28,7 @@ public:
         }
         return mat;
     }
+    
+    //similar, but cleaner approach: add each element into its diagonal's priority queue
+    //link: https://leetcode.com/problems/sort-the-matrix-diagonally/discuss/489749/JavaPython-Straight-Forward
 };
